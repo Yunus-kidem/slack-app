36eba5dca73437405ce66690325eaf49f1d0d31e
unknown <yunuskt363@gmail.com>
2022-03-05 01:55:16 +0300

    Finished up with the sidebar Header section

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,22 @@
+import { Create, FiberManualRecord } from '@mui/icons-material';
 import React from 'react'
 import './Sidebar.css';
 
 function Sidebar() {
-  return (
-    <div className="sidebar"></div>
-  )
+return (
+    <div className="sidebar">
+        <div className="sidebar_header">
+            <div className="sidebar_info">
+            <h2>Slack Programmer</h2>
+            <h3>
+                <FiberManualRecord/>
+                Yunus Kidem
+            </h3>   
+            </div>
+            <Create/>
+        </div>
+    </div>
+)
 }
 
 export default Sidebar;
