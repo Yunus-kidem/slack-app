32c9540988144166846e86f41d8a69f68d37488c
unknown <yunuskt363@gmail.com>
2022-03-05 14:41:53 +0300

    Finished up the above 50% of Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
-import { Create, FiberManualRecord } from '@mui/icons-material';
+import { Add, Apps, BookmarkBorder, Create, Drafts, ExpandLess, ExpandMore, FiberManualRecord, FileCopy, Inbox, InsertComment, PeopleAlt } from '@mui/icons-material';
 import React from 'react'
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
 
 function Sidebar() {
 return (
@@ -15,6 +16,19 @@ return (
             </div>
             <Create/>
         </div>
+        <SidebarOption Icon={InsertComment} title="Threads" />
+        <SidebarOption Icon={Inbox} title="Mentions & reactions" />
+        <SidebarOption Icon={Drafts} title="Saved items" />
+        <SidebarOption Icon={BookmarkBorder} title="Channel browser" />
+        <SidebarOption Icon={PeopleAlt} title="People & user groups" />
+        <SidebarOption Icon={Apps} title="Apps" />
+        <SidebarOption Icon={FileCopy} title="File browser" />
+        <SidebarOption Icon={ExpandLess} title="Show less" />
+        <hr/>
+        <SidebarOption Icon={ExpandMore} title="Channels" />
+        <hr/>
+        <SidebarOption Icon={Add} title="Add Channel" />
+        <SidebarOption title="Stay-motivated" />
     </div>
 )
 }
